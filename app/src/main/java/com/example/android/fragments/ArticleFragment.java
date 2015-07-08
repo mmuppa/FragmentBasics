58e4a6d6436742d3cdb9afd8128dd42396380e91
mmuppa <mmuppa@uw.edu>
2015-07-08 14:32:17 -0700

    Fixed the classes to use the latest api rather than the api4 support libraries.

--- app/src/main/java/com/example/android/fragments/ArticleFragment.java
+++ app/src/main/java/com/example/android/fragments/ArticleFragment.java
@@ -15,7 +15,7 @@
  */
 package com.example.android.fragments;
 
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/example/android/fragments/HeadlinesFragment.java
+++ app/src/main/java/com/example/android/fragments/HeadlinesFragment.java
@@ -18,7 +18,7 @@ package com.example.android.fragments;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.app.ListFragment;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
